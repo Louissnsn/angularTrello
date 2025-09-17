dc7b644910b70d5a012614bb29da225719e3b8a0
Loulou <sanson.louis@gmail.com>
2025-09-17 12:30:41 +0200

    initialize app, building routes, add lazyloading conf, setting up in standalone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage, pathMatch: 'full' },
+  { path: 'auth', loadChildren: () => import('./auth/auth.routes').then((m) => m.routes) },
+  { path: '**', redirectTo: '' }, // fallback
+];
